33a00d459c9592a756df8c9b728294bb2bb2390c
Marcos França <marcos.franca@freterapido.com>
2022-10-03 09:57:27 -0300

    files

--- app/Http/Controllers/ApiControllerV2.php
+++ app/Http/Controllers/ApiControllerV2.php
@@ -17,7 +17,6 @@ class ApiControllerV2 extends Controller
         try {
             $data = $request->all();
             foreach ($data as $e) {
-                dd($e);
                 $res = $request->user()->empresas()->updateOrCreate([
                     'iderp' => $e['iderp'],
                     'user_id' => $request->user()->id
@@ -28,6 +27,7 @@ class ApiControllerV2 extends Controller
                     'iderp' => $e['iderp'],
                     'user_id' => $request->user()->id
                 ]);
+                dd($res);
             }
             return response()->json(null, Response::HTTP_CREATED);
         } catch (Exception $e) {
