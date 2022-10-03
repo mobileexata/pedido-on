edf7947bcbc4a3b82ad6885b0a26f6d5401dff8c
Marcos França <marcos.franca@freterapido.com>
2022-10-03 09:43:56 -0300

    files

--- app/Http/Middleware/AuthApiV2.php
+++ app/Http/Middleware/AuthApiV2.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthApiV2
 {   
+    const X_API_TOKEN = 'X-API-TOKEN';
     /**
      *
      * @param \Illuminate\Http\Request $request
@@ -20,11 +21,11 @@ class AuthApiV2
      */
     public function handle(Request $request, Closure $next)
     {
-        if (!$request->hasHeader('X-API-TOKEN')) {
+        if (!$request->hasHeader(self::X_API_TOKEN)) {
             return response()->json(['error' => 'api token is required'], Response::HTTP_UNAUTHORIZED);
         }
 
-        $user = User::where('user_token', $request->header('X-API-TOKEN'))->first();
+        $user = User::where('user_token', $request->header(self::X_API_TOKEN))->first();
         if (!$user) {
             return response()->json(['error' => 'user not found'], Response::HTTP_UNAUTHORIZED);
         }
